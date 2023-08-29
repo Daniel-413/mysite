d9c830f93f6eaf500cc7bdad0345bd78fee66969
Daniel Rodrigues <rodriguesdaniel51108@gmail.com>
2023-08-29 12:21:42 -0300

    refactor: altera a lógica de votação na QuestionView

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', IndexView.as_view(),name='index'),
     path('question/<int:pk>/', QuestionView.as_view(),name='question'),
     path('question/<int:pk>/results/', ResultsView.as_view(), name='results'),
-    path("question/<int:pk>/vote/", vote, name="vote"),
+   
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from .models import *
@@ -13,10 +13,22 @@ class QuestionView(DetailView):
     model = Question
     template_name = "question.html"
 
+    def post(self, request, *args, **kwargs):
+        question = self.get_object()
+        choice_id = request.POST.get('choice')
+        selected_choice = question.choice_set.get(pk=choice_id)
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('results', question.id)
+
+
 class ResultsView(DetailView):
     model = Question
     template_name = "results.html"
+    
 
+
+'''
 def vote(request, pk):
     question = get_object_or_404(Question, pk=pk)
     try:
@@ -38,3 +50,4 @@ def vote(request, pk):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("results", args=(question.id,)))
+'''
